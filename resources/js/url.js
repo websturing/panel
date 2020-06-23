d4c5f232e0baecaaedc4c934ed7d5c384caa4178
websturing <websturing.project@gmail.com>
2020-06-23 11:45:50 +0700

    setup vuejs

---
+++ resources/js/url.js
@@ -0,0 +1,19 @@
+const urlAxios = "http://localhost/ptsp_pemohon/api";
+const urlBase = "ptsp_pemohon";
+const urlWeb = "http://localhost/ptsp_pemohon";
+const assets = "http://localhost/ptsp_pemohon/public/images";
+const perusahaanFile = "http://localhost/Resources/perusahaan";
+
+// const urlAxios = "http://pemohon.appkepri.com/api";
+// const urlBase = "";
+// const urlWeb = "http://pemohon.appkepri.com";
+// const assets = "http://pemohon.appkepri.com/public/images";
+// const perusahaanFile = "http://appkepri.com/Resources/perusahaan";
+
+export default {
+    urlAxios: urlAxios,
+    urlBase: urlBase,
+    urlWeb: urlWeb,
+    perusahaanFile: perusahaanFile,
+    assets: assets,
+}
